ba994b337c24638f90a74923dfc83c528bb3c690
Jialei Wang <wang2700@purdue.edu>
2021-01-26 07:10:02 -0500

    Complete MATLAB code for FIR Sharpening Filter

---
+++ Lab1/src/fir_sharpen_filter.m
@@ -0,0 +1,28 @@
+close all
+h = ones(5, 5) * 1/25;
+
+colormap(parula(64))
+[H, f1, f2] = freqz2(h);
+f1 = f1 * pi;
+f2 = f2 * pi;
+mesh(f1, f2, abs(H))
+axis ([-pi pi -pi pi 0 1])
+xlabel('f_1 (rad)')
+ylabel('f_2 (rad)')
+zlabel('magnitude')
+title('Frequency Response of FIR Low Pass Filter')
+
+figure
+delta = zeros(5,5);
+delta(3,3) = 1;
+lambda = 1.5;
+g = delta + lambda .* (delta - h);
+[G, f1, f2] = freqz2(g);
+f1 = f1 * pi;
+f2 = f2 * pi;
+mesh(f1, f2, abs(G))
+axis ([-pi pi -pi pi 1 3])
+xlabel('f_1 (rad)')
+ylabel('f_2 (rad)')
+zlabel('magnitude')
+title('Frequency Response of FIR Sharpening Filter')
